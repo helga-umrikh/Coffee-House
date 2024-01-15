ed91043111614fc4058e8e6c452651c02d5d2efe
helga-umrikh <zetaumr@gmail.com>
2024-01-15 03:45:26 +0300

    fix: closing burger menu when nav btn is clicked

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,9 +1,17 @@
 window.onload = function () {
   const menu_btn = document.querySelector('.header__hamburger');
-  const mobile_menu = document.querySelector('.mobile-header')
+  const mobile_menu = document.querySelector('.mobile-header');
+  const nav_items = document.querySelectorAll('.nav_item');
 
   menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+  });
+
+  nav_items.forEach(function (nav_item) {
+    nav_item.addEventListener('click', function () {
+      menu_btn.classList.toggle('is-active');
+      mobile_menu.classList.toggle('is-active');
+    });
   });
 }
