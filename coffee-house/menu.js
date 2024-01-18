2466cef71c8d511efb0dcde09a993c88d27b1b5e
helga-umrikh <zetaumr@gmail.com>
2024-01-19 00:54:08 +0300

    feat: add dynamicy menu

--- coffee-house/menu.js
+++ coffee-house/menu.js
@@ -1,52 +1,73 @@
-import coffeeData from './coffeeData.js';
+import coffeeData from './coffeeData.js'
+import teaData from './teaData.js';
+import desertData from './desertsData.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+function renderCards(data) {
+    const section = document.querySelector('.main__menu-cards-section')
+    const menuContainer = document.createElement('div')
+    menuContainer.classList.add('menu-cards__container')
+    section.appendChild(menuContainer)
 
-const coffeeSection = document.querySelector('._coffee');
-const menuContainer = document.createElement('div');
-menuContainer.classList.add('menu-cards__container');
-coffeeSection.appendChild(menuContainer);
-  
+    data.forEach((item) => {
+        const menuCardsItem = document.createElement('div')
+        menuCardsItem.classList.add('menu-cards__item')
 
-coffeeData.forEach(item => {
+        const menuCardsHeader = document.createElement('div')
+        menuCardsHeader.classList.add('menu-cards__header')
 
-  const menuCardsItem = document.createElement('div');
-  menuCardsItem.classList.add('menu-cards__item');
+        const imageElement = document.createElement('img')
+        imageElement.classList.add('menu-cards__image')
+        imageElement.src = item.src
+        imageElement.alt = 'coffee drink example'
 
-  const menuCardsHeader = document.createElement('div');
-  menuCardsHeader.classList.add('menu-cards__header');
+        const menuCardsDescription = document.createElement('div')
+        menuCardsDescription.classList.add('menu-cards__description')
 
-  const imageElement = document.createElement('img');
-  imageElement.classList.add('menu-cards__image');
-  imageElement.src = item.src;
-  imageElement.alt = 'coffee drink example';
+        const titleElement = document.createElement('p')
+        titleElement.classList.add('menu-cards__title')
+        titleElement.textContent = item.title
 
+        const subtitleElement = document.createElement('p')
+        subtitleElement.classList.add('menu-cards__subtitle', 'subtitle')
+        subtitleElement.textContent = item.description
 
+        const costElement = document.createElement('p')
+        costElement.classList.add('menu-cards__title', 'menu-cards__cost')
+        costElement.textContent = item.cost
 
-  const menuCardsDescription = document.createElement('div');
-  menuCardsDescription.classList.add('menu-cards__description');
+        menuContainer.appendChild(menuCardsItem)
+        menuCardsItem.appendChild(menuCardsHeader)
+        menuCardsHeader.appendChild(imageElement)
 
-  const titleElement = document.createElement('p');
-  titleElement.classList.add('menu-cards__title');
-  titleElement.textContent = item.title;
+        menuCardsItem.appendChild(menuCardsDescription)
+        menuCardsDescription.appendChild(titleElement)
+        menuCardsDescription.appendChild(subtitleElement)
+        menuCardsDescription.appendChild(costElement)
+    })
+}
 
-  const subtitleElement = document.createElement('p');
-  subtitleElement.classList.add('menu-cards__subtitle', 'subtitle');
-  subtitleElement.textContent = item.description;
+window.onload = function() {
+  renderCards(coffeeData);
+}
 
-  const costElement = document.createElement('p');
-  costElement.classList.add('menu-cards__title', 'menu-cards__cost');
-  costElement.textContent = item.cost;
+const coffeeBtn = document.querySelector('._coffee-btn');
+const teaBtn = document.querySelector('._tea-btn');
+const desertBtn = document.querySelector('._desert-btn')
 
-
-  menuContainer.appendChild(menuCardsItem);
-  menuCardsItem.appendChild(menuCardsHeader);
-  menuCardsHeader.appendChild(imageElement);
-
-  menuCardsItem.appendChild(menuCardsDescription);
-  menuCardsDescription.appendChild(titleElement);
-  menuCardsDescription.appendChild(subtitleElement);
-  menuCardsDescription.appendChild(costElement);
-
+coffeeBtn.addEventListener('click', function () {
+    const container = document.querySelector('.menu-cards__container')
+    container && container.remove('menu-cards__container')
+    renderCards(coffeeData)
 })
-}) 
+
+teaBtn.addEventListener('click', function () {
+    const container = document.querySelector('.menu-cards__container')
+    container && container.remove('.menu-cards__container')
+    renderCards(teaData)
+})
+
+desertBtn.addEventListener('click', function () {
+  const container = document.querySelector('.menu-cards__container')
+  container && container.remove('.menu-cards__container')
+  renderCards(desertData)
+})
---
+++ coffee-house/teaData.js
@@ -0,0 +1,30 @@
+const teaData = [
+  {
+    src: "./assets/menu/tea/tea-1.png",
+    title: 'Moroccan',
+    description: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
+    cost: "$4.50"
+  },
+  {
+    src: "./assets/menu/tea/tea-2.png",
+    title: "Ginger",
+    description: "Original black tea with fresh ginger, lemon and honey",
+    cost: "$5.00"
+  },
+  {
+    src: "./assets/menu/tea/tea-3.png",
+    title: "Cranberry",
+    description: "Invigorating black tea with cranberry and honey",
+    cost: "$5.00"
+  },
+
+  {
+    src: "./assets/menu/tea/tea-4.png",
+    title: "Sea buckthorn",
+    description: "Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon",
+    cost: "$5.50"
+  },
+]
+
+
+export default teaData;
