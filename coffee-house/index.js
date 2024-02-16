f2d0745d2c7536777ce90ada39b49cd27b005c72
helga-umrikh <zetaumr@gmail.com>
2024-02-16 10:33:03 +0300

    feat: autoscroll and progress bar in slider

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -48,27 +48,29 @@ const sliderData = [
 document.addEventListener('DOMContentLoaded', function () {
     const prevButton = document.querySelector('.prev-slide')
     const nextButton = document.querySelector('.next-slide')
-    const dashButtons = document.querySelectorAll('.dash__button')
+    const progressDashes = document.querySelectorAll('progress')
 
     let slideIndex = 0
+    let duration = 5000
+    let intervalSlideID
+    let intervalProgressID
 
-    prevButton && prevButton.addEventListener('click', () => {
-        slideIndex === 0 ? (slideIndex = 2) : slideIndex--
-        updateSliderContent()
-    })
-
-    nextButton && nextButton.addEventListener('click', () => {
-        slideIndex === 2 ? (slideIndex = 0) : slideIndex++
-        updateSliderContent()
-    })
-
-    dashButtons && dashButtons.forEach((e, index) => {
-        e.addEventListener('click', () => {
-            slideIndex = index
-            updateSliderContent()
-            e.classList.toggle('active_dash')
+    prevButton &&
+        prevButton.addEventListener('click', () => {
+            updateSliderIndex(false)
+            updateCurrentSlide()
         })
-    })
+
+    nextButton &&
+        nextButton.addEventListener('click', () => {
+            updateSliderIndex(true)
+            updateCurrentSlide()
+        })
+
+    function updateSliderIndex(direction) {
+        direction || (slideIndex === 0 ? (slideIndex = 2) : slideIndex--)
+        direction && (slideIndex === 2 ? (slideIndex = 0) : slideIndex++)
+    }
 
     function updateSliderContent() {
         const sliderImg = document.querySelector('.slider_src')
@@ -84,25 +86,53 @@ document.addEventListener('DOMContentLoaded', function () {
         sliderPrice.textContent = currentSlide.price
     }
 
-    updateSliderContent()
-})
+    function fillSliderProgress(index) {
+        let counterFillValue = 0
+        let progressDuration = Math.floor(duration / 100)
+        let currentProgressDash = progressDashes[index]
 
+        if (intervalProgressID) {
+            clearInterval(intervalProgressID)
+        }
+
+        intervalProgressID = setInterval(() => {
+            if (counterFillValue >= 100) {
+                clearInterval(intervalProgressID)
+                counterFillValue = 0
+                currentProgressDash.value = counterFillValue
+                updateSliderIndex(true)
+                updateCurrentSlide()
+            }
+            currentProgressDash.value = counterFillValue
+            counterFillValue++
+        }, progressDuration)
+    }
+
+    function updateCurrentSlide() {
+        updateSliderContent()
+        fillSliderProgress(slideIndex)
+    }
+
+    updateCurrentSlide()
+})
 
 //navigation from MenuPage to sections of home page
 
-const favoriteButton = document.getElementById('favorite');
-const aboutButton = document.getElementById('about');
-const mobileAppButton = document.getElementById('mobile-app');
+const favoriteButton = document.getElementById('favorite')
+const aboutButton = document.getElementById('about')
+const mobileAppButton = document.getElementById('mobile-app')
 
+favoriteButton &&
+    favoriteButton.addEventListener('click', function () {
+        window.location.href = 'index.html#favorite-section'
+    })
 
-favoriteButton && favoriteButton.addEventListener('click', function () {
-    window.location.href = 'index.html#favorite-section'
-})
+aboutButton &&
+    aboutButton.addEventListener('click', function () {
+        window.location.href = 'index.html#about-section'
+    })
 
-aboutButton && aboutButton.addEventListener('click', function () {
-    window.location.href = 'index.html#about-section'
-})
-
-mobileAppButton && mobileAppButton.addEventListener('click', function () {
-    window.location.href = 'index.html#mobile-app-section'
-})
+mobileAppButton &&
+    mobileAppButton.addEventListener('click', function () {
+        window.location.href = 'index.html#mobile-app-section'
+    })
